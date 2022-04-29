b8a950eea8fe18c956a787e285384105bf06fd70
AlttiRi <16310547+AlttiRi@users.noreply.github.com>
2022-04-29 19:38:54 +0300

    Update `setImmediateLike`

--- src/sleep.js
+++ src/sleep.js
@@ -1,11 +1,11 @@
-const setImmediate = globalThis.setImmediate || /*#__PURE__*/ (function setImmediateLike() {
+const setImmediate = globalThis.setImmediate || /*#__PURE__*/ (function() {
     const {port1, port2} = new MessageChannel();
     const queue = [];
     port1.onmessage = function() {
         const callback = queue.shift();
         callback();
     };
-    return function(callback) { // Simplified implementation: only callback argument.
+    return function setImmediateLike(callback) { // Simplified implementation: only callback argument.
         port2.postMessage(null);
         queue.push(callback);
     };
