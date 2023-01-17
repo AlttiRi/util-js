d0bf7491e4a9e2d7db36ab82eb63e57c356fda04
AlttiRi <16310547+AlttiRi@users.noreply.github.com>
2023-01-17 20:06:02 +0300

    Fix `Cannot access 'setImmediate' before initialization`

--- src/sleep.js
+++ src/sleep.js
@@ -1,4 +1,4 @@
-const __setImmediate = typeof setImmediate === "function" ? setImmediate : null;
+const __setImmediate = typeof globalThis.setImmediate === "function" ? globalThis.setImmediate : null;
 const setImmediate = __setImmediate || /*#__PURE__*/ (function() {
     const {port1, port2} = new MessageChannel();
     const queue = [];
@@ -12,9 +12,21 @@ const setImmediate = __setImmediate || /*#__PURE__*/ (function() {
     };
 })();
 
+/**
+ * Sleeps `ms` milliseconds.
+ * If param is `undefined` it sleeps until the next macro-task.
+ * Note: with `0` real ms will be `4` ms.
+ * @param {number?} ms
+ * */
 export function sleep(ms) {
     if (ms === undefined) {
         return new Promise(resolve => setImmediate(resolve));
     }
     return new Promise(resolve => setTimeout(resolve, ms));
 }
+
+console.time("1");
+for (let i = 0; i < 1000; i++) {
+    await sleep();
+}
+console.timeEnd("1");
