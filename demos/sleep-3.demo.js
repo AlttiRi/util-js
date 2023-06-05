dfe3ea570bff9681d7fb5953556d0d05e9582fd4
AlttiRi <16310547+AlttiRi@users.noreply.github.com>
2023-06-05 08:08:03 +0300

    Update demo

--- demos/sleep-3.demo.js
+++ demos/sleep-3.demo.js
@@ -4,7 +4,7 @@ console.time("sleep()");
 for (let i = 0; i < 100; i++) {
     await sleep();
 }
-console.timeEnd("sleep()"); // ~1.5ms
+console.timeEnd("sleep()"); // ~1.5 ms // 0.0015 s
 
 
 // ~1000 times slower
@@ -12,24 +12,24 @@ console.time("sleep(0)");
 for (let i = 0; i < 100; i++) {
     await sleep(0);
 }
-console.timeEnd("sleep(0)"); // ~1.5s
+console.timeEnd("sleep(0)"); // ~1.5 s
 
 
 console.time("sleep(1)");
 for (let i = 0; i < 100; i++) {
     await sleep(1);
 }
-console.timeEnd("sleep(1)"); // ~1.5s
+console.timeEnd("sleep(1)"); // ~1.5 s
 
 
 console.time("sleep(4)");
 for (let i = 0; i < 100; i++) {
     await sleep(4);
 }
-console.timeEnd("sleep(4)"); // ~1.5s
+console.timeEnd("sleep(4)"); // ~1.5 s
 
 console.time("sleep(10)");
 for (let i = 0; i < 100; i++) {
     await sleep(10);
 }
-console.timeEnd("sleep(10)"); // ~1.5s
+console.timeEnd("sleep(10)"); // ~1.5 s
