b97150f7877af101b0bef887ebee1cebaea188eb
AlttiRi <16310547+AlttiRi@users.noreply.github.com>
2024-06-03 03:58:33 +0300

    Simplify `isString`

--- src/util.ts
+++ src/util.ts
@@ -1,3 +1,6 @@
 export function isString(value: unknown): value is string {
+    return typeof value === "string";
+}
+export function isAnyString(value: unknown): value is (string | String) {
     return typeof value === "string" || value instanceof String;
 }
