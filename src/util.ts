9f90d1d9e3a570f233ebb9a7f1bcda028819c2f8
AlttiRi <16310547+AlttiRi@users.noreply.github.com>
2024-07-14 00:32:46 +0300

    Add `hashString`

--- src/util.ts
+++ src/util.ts
@@ -4,3 +4,20 @@ export function isString(value: unknown): value is string {
 export function isAnyString(value: unknown): value is (string | String) {
     return typeof value === "string" || value instanceof String;
 }
+/**
+ * Java's `hashCode` like.
+ * @example
+ * hashString("Lorem Ipsum") === -488052133
+ * hashString("Qwerty") === -1862984904
+ * hashString("A") === 65
+ * hashString("👾👽💀") === -2019372252
+ * @param {string} str
+ * @return {number}
+ */
+export function hashString(str: string): number {
+    let hash = 0;
+    for (let i = 0; i < str.length; i++) {
+        hash = Math.imul(Math.imul(31, hash) + str.charCodeAt(i), 1);
+    }
+    return hash;
+}
