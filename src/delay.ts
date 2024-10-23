aba6dd217df091dde991bfe8c36d4954f7831d85
AlttiRi <16310547+AlttiRi@users.noreply.github.com>
2024-10-23 09:53:38 +0300

    Update JSDocs for delay functions

--- src/delay.ts
+++ src/delay.ts
@@ -1,5 +1,19 @@
-/** A classic `debounce` wrap function. */
-export function debounce<A extends any[]>(runnable: (...args: A) => unknown, ms = 50, scope?: any): (...args: A) => void {
+/**
+ * A classic `debounce` wrap function.
+ *
+ * `ms` param is `250` ms by default.
+ *
+ * @example
+ * const logAny = (i: any) => console.log(i);
+ * const logAnyDebounced = debounce(logAny, 50);
+ *
+ * // prints `99`, after ~ 1+ second
+ * for (let i = 0; i < 100; i++) {
+ *     logAnyDebounced(i);
+ *     await sleep(10);
+ * }
+ */
+export function debounce<A extends any[]>(runnable: (...args: A) => unknown, ms = 250, scope?: any): (...args: A) => void {
     let timerId: number | undefined;
     return function debounced(this: any): void {
         if (timerId !== undefined) {
@@ -13,8 +27,25 @@ export function debounce<A extends any[]>(runnable: (...args: A) => unknown, ms 
     }
 }
 
-/** A classic `throttle` wrap function. */
-export function throttle<A extends any[]>(runnable: (...args: A) => any, time = 50, scope?: any): (...args: A) => void {
+/**
+ * A classic `throttle` wrap function.
+ *
+ * Executes the wrapped function no more than once per `ms`.
+ * The first and last calls will always be performed.
+ *
+ * `ms` param is `250` ms by default.
+ *
+ * @example
+ * const logAny = (i: any) => console.log(i);
+ * const logAnyThrottled = throttle(logAny, 500);
+ *
+ * // prints "0 31 63 96 99" // "31 63 96" may differ
+ * for (let i = 0; i < 100; i++) {
+ *     logAnyThrottled(i);
+ *     await sleep(10);
+ * }
+ */
+export function throttle<A extends any[]>(runnable: (...args: A) => any, ms = 250, scope?: any): (...args: A) => void {
     let waiting = false;
     let queued  = false;
     let context: any;
@@ -22,7 +53,7 @@ export function throttle<A extends any[]>(runnable: (...args: A) => any, time = 
 
     function delayed() {
         if (queued) {
-            setTimeout(delayed, time);
+            setTimeout(delayed, ms);
             runnable.apply(scope || context, args);
         } else {
             waiting = false;
@@ -33,7 +64,7 @@ export function throttle<A extends any[]>(runnable: (...args: A) => any, time = 
     return function(this: any, ...current_args: A) {
         if (!waiting) {
             waiting = true;
-            setTimeout(delayed, time);
+            setTimeout(delayed, ms);
             runnable.apply(scope || this, current_args);
         } else {
             context = this;
@@ -46,9 +77,12 @@ export function throttle<A extends any[]>(runnable: (...args: A) => any, time = 
 /**
  * Allows to run a function as a `throttled` one, run it without a delay (`runNow`), or `clear` the deferred callback.
  *
+ * `ms` param is `250` ms by default.
+ *
  * @example
  * const {throttled, runNow, clear} = getThrottle(300, true);
  *
+ * // prints "0 20 40 60 80 99"
  * for (let i = 0; i < 100; i++) {
  *     throttled(() => {
  *         console.log(i);
@@ -57,7 +91,7 @@ export function throttle<A extends any[]>(runnable: (...args: A) => any, time = 
  * }
  * runNow(); // run the last callback without delay
  */
-export function getThrottle(ms = 1000, runFirstImmediately = true) {
+export function getThrottle(ms = 250, runFirstImmediately = true) {
     let cb: Function;
     let timer: number | undefined;
     function throttled(callback: Function, runNow = false) {
@@ -87,3 +121,9 @@ export function getThrottle(ms = 1000, runFirstImmediately = true) {
     }
     return {throttled, runNow: execNow, clear};
 }
+
+// todo: in `getThrottle`
+// function throttledCall(this: any, callback: Function, ...params: any[]) {
+//     cb = callback.bind(this, ...params);
+//     ...
+// }
